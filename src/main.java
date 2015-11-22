c758fdff0158501e65cc6d18d06aaff22afc288c
saoliva <saoliva@uc.cl>
2015-11-22 16:15:20 -0300

    Agregado lo basico del query parser

--- src/main.java
+++ src/main.java
@@ -3,10 +3,12 @@ public class main {
 
 	public static void main(String[] args) {
 		
-		String neo4j_query = ""; // aqui ira una query neo4j para probar
+		String neo4j_query = "MATCH (m:Movie) RETURN m"; // aqui ira una query neo4j para probar
+		String neo4j_query2 = "MATCH (p:Person { name:\"Keanu Reeves\"}) RETURN p"; // aqui ira una query neo4j para probar
+				
 		
 		QueryParser qp = new QueryParser();
-		qp.recieveQuery("hola");
+		qp.recieveQuery(neo4j_query2);
 		
 		
 
